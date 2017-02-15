78f30b7b0d4bb0dc25d5518772a3616f782c02b2
Frank Febbraro <frank@phase2technology.com>
2017-02-14 16:47:54 -0800

    more autocomplete madness

--- Formula/rig.rb
+++ Formula/rig.rb
@@ -3,7 +3,7 @@ class Rig < Formula
   homepage "https://outrigger.sh"
   url "https://s3.amazonaws.com/phase2.outrigger/rig-1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "bd24474416adb5de906c1880492a15b51cccce3c4d28a979de7b2bcf6949dd15"
+  sha256 "d8bc3e794fe1d9d255d2747c651f9fd100cb2587538e89e09e06c1c9e0c9f6ba"
 
   depends_on "docker"
   depends_on "docker-machine"
@@ -13,7 +13,6 @@ class Rig < Formula
   def install
     bin.install "rig"
     bin.install "docker-machine-watch-rsync.sh"
-    bin.install "rig_completer"
 
     bash_completion.install "bash_autocomplete" => "rig"
     zsh_completion.install "zsh_autocomplete" => "_rig"
