c712aa596f21b194bbd2aaea005fe397a7dc3237
Frank Febbraro <frank@phase2technology.com>
2017-02-14 16:44:31 -0800

    more autocomplete madness

--- Formula/rig.rb
+++ Formula/rig.rb
@@ -3,7 +3,7 @@ class Rig < Formula
   homepage "https://outrigger.sh"
   url "https://s3.amazonaws.com/phase2.outrigger/rig-1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "e122578a5e3cf9460afdfcbd774cb30c890a7bc6d018e9de34a8b622577801eb"
+  sha256 "bd24474416adb5de906c1880492a15b51cccce3c4d28a979de7b2bcf6949dd15"
 
   depends_on "docker"
   depends_on "docker-machine"
