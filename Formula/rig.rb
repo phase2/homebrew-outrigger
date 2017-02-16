04806a5dee4550422883848815e83dc5a8f7970e
Frank Febbraro <frank@phase2technology.com>
2017-02-16 14:50:37 -0800

    another refresh

--- Formula/rig.rb
+++ Formula/rig.rb
@@ -3,7 +3,7 @@ class Rig < Formula
   homepage "https://outrigger.sh"
   url "https://s3.amazonaws.com/phase2.outrigger/rig-1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "2b6141bd8e7c82dd3ed2993a4ea3755b6642d724582616103b75aae90cb12e75"
+  sha256 "8243eb3e9f90c02c9675aa203ff35fce6c8bddf8ec70413d051129176d961c54"
 
   depends_on "docker"
   depends_on "docker-machine"
