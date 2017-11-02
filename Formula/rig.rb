b7cfc2ac9c9415041d1ee11b9b1dd4d91db6f9b0
Frank Febbraro <frankfebbraro@gmail.com>
2017-11-02 14:43:13 -0700

    Added devel release

--- Formula/rig.rb
+++ Formula/rig.rb
@@ -5,13 +5,18 @@ class Rig < Formula
   version "1.3.2"
   sha256 "82dd773501335b20d462092654cf5e612d7df4aa16e81657fd0da7408d6b8225"
 
+  devel do
+    url "https://s3.amazonaws.com/phase2.outrigger/rig-2.0.0-rc1-macOS-amd64.tar.gz"
+    sha256 "e50551adbdf0ade1207ec61bd64784df5d35c5a4b2c4f21cd0ba4833a3e081e5"
+  end
+
   depends_on "docker"
   depends_on "docker-machine"
   depends_on "docker-compose"
   depends_on "docker-machine-nfs"
   depends_on "unison"
   depends_on "eugenmayer/dockersync/unox"
-
+  
   def install
     bin.install "rig"
     bin.install "docker-machine-watch-rsync.sh"
