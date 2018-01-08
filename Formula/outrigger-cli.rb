789907b514971eb20870ad17cc6bd82e311202b8
Outrigger <outrigger@phase2technology.com>
2018-01-08 15:19:06 -0800

    outrigger-cli version 2.1.0

--- Formula/outrigger-cli.rb
+++ Formula/outrigger-cli.rb
@@ -1,9 +1,9 @@
 class OutriggerCli < Formula
   desc "Containerized development environment for projects. See https://docs.outrigger.sh for documentation."
   homepage "https://outrigger.sh/"
-  url "https://github.com/phase2/rig/releases/download/2.0.0/outrigger-cli-2.0.0-macOS-amd64.tar.gz"
-  version "2.0.0"
-  sha256 "cb70e9b9356deaac7a9ccef1788a2984749e1e2f7ff1d9f230a68c017880cdc0"
+  url "https://github.com/phase2/rig/releases/download/2.1.0/outrigger-cli-2.1.0-macOS-amd64.tar.gz"
+  version "2.1.0"
+  sha256 "6fdba4bce923de641bab28efdd8a9bd81289899026e79a90a1d6bce3d34b0514"
   
   depends_on "docker"
   depends_on "docker-machine"
