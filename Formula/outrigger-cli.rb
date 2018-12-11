62b8818a50b48ec2c91e820cb1dabb4d555bf77d
Outrigger <outrigger@phase2technology.com>
2018-12-11 15:31:46 -0800

    Brew formula update for outrigger-cli version 2.2.2

--- Formula/outrigger-cli.rb
+++ Formula/outrigger-cli.rb
@@ -1,9 +1,9 @@
 class OutriggerCli < Formula
   desc "Containerized development environment for projects. See https://docs.outrigger.sh for documentation."
   homepage "https://outrigger.sh/"
-  url "https://github.com/phase2/rig/releases/download/2.2.1/outrigger-cli-2.2.1-macOS-amd64.tar.gz"
-  version "2.2.1"
-  sha256 "b4c7acd42ae09e5e8e0ab0d33068918dbe9d59a47417adf027496d51be744056"
+  url "https://github.com/phase2/rig/releases/download/2.2.2/outrigger-cli-2.2.2-macOS-amd64.tar.gz"
+  version "2.2.2"
+  sha256 "2c609b8d32dc279d66f531a3eaa44e4ab6a18946d7a0994cbeac06183f95e9ee"
   
   depends_on "docker"
   depends_on "docker-machine"
